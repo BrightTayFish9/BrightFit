8c8563aca24ce25ac470c7e1a6f9559a7bc119d4
BrightTayFish9 <Fitness.Bright9@gmail.com>
2022-09-17 08:40:36 -0600

    flutter updates and iOS running - austin duran

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/austinduran/brighton/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/austinduran/brighton/projects/BrightFit"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/austinduran/brighton/projects/BrightFit/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/austinduran/brighton/projects/BrightFit/.dart_tool/package_config.json"
